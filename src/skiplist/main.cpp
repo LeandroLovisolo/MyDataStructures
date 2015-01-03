1f595375b95e6008e84913fe12b4a099671ae15e
Leandro Lovisolo <leandro@leandro.me>
2015-01-03 09:30:25 -0300

    Website distribution target

---
+++ src/skiplist/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <ctime>
+#include <algorithm>
+
+#include "js.h"
+#include "skiplist.h"
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////
+// Javascript interface                                                       //
+////////////////////////////////////////////////////////////////////////////////
+
+#ifdef EMSCRIPTEN
+
+skiplist s;
+
+extern "C" {
+  void EMSCRIPTEN_KEEPALIVE skiplist_insert(int x) {
+    s.insert(x);
+  }
+
+  void EMSCRIPTEN_KEEPALIVE skiplist_remove(int x) {
+    s.remove(x);
+  }
+
+  const char * EMSCRIPTEN_KEEPALIVE skiplist_print() {
+    return s.print().c_str();
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Terminal interface                                                         //
+////////////////////////////////////////////////////////////////////////////////
+
+#else
+
+#define NUM_ITEMS_DEFAULT 10
+
+vector<int> random_items(int num_items) {
+  vector<int> items(num_items);
+  for(int i = 0; i < num_items; i++) { 
+    items[i] = i + 1;
+  }
+  srand(time(NULL));
+  random_shuffle(items.begin(), items.end());
+  return items;
+}
+
+skiplist build_skiplist(vector<int> &items) {
+  skiplist l;
+  cout << "Empty skip list:" << endl;
+  cout << l.print();
+
+  for(int i : items) {
+    cout << endl << "Inserting " << i << ":" << endl;
+    l.insert(i);
+    cout << l.print();
+  }
+  return l;
+}
+
+void find_items(skiplist &l, vector<int> &items) {
+  for(int i : items) {
+    cout << "find(" << i << "):\t" << l.find(i) << endl;
+  }
+}
+
+void remove_items(skiplist &l, vector<int> &items) {
+  for(int i : items) {
+    cout << endl << "Removing " << i << ":" << endl;
+    l.remove(i);
+    cout << l.print();
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int num_items = (argc == 1 ? NUM_ITEMS_DEFAULT : atoi(argv[1]));
+
+  vector<int> items = random_items(num_items);
+
+  cout << "Items:" << endl;
+  for(int i : items) cout << i << " ";
+  cout << endl;
+
+  cout << endl << "Building skip list..." << endl;
+  skiplist l = build_skiplist(items);
+
+  cout << endl << "Finding elements:" << endl;
+  find_items(l, items);
+
+  cout << endl << "Removing elements:" << endl;
+  remove_items(l, items);
+
+  return 0;
+}
+
+#endif
---
+++ src/skiplist/skiplist.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <algorithm>
+#include "skiplist.h"
+
+using namespace std;
+
+skiplist::skiplist() : head(0), tail(0) {
+  head.forward.push_back(&tail);
+}
+
+skiplist::~skiplist() {
+  node *current = head.forward[0];
+  while(current != &tail) {
+    node *old = current;
+    current = current->forward[0];
+    delete old;
+  }
+}
+
+bool skiplist::find(int value) {
+  node *current = &head;
+  for(int level = head.level(); level >= 0; level--) {
+    while(current->forward[level] != &tail &&
+          current->forward[level]->value <= value) {
+      current = current->forward[level];
+      if(current->value == value) return true;
+    }
+  }
+  return false;
+}
+
+void skiplist::insert(int value) {
+  vector<node*> update(head.level() + 1);
+  node *current = &head;
+  for(int level = head.level(); level >= 0; level--) {
+    while(current->forward[level] != &tail &&
+          current->forward[level]->value <= value) {
+      current = current->forward[level];
+    }
+    update[level] = current;
+  }
+  if(current->forward[0]->value == value) return;
+
+  int level = randomLevel();
+  if(level > head.level()) {
+    for(int i = head.level() + 1; i <= level; i++) {
+      update.push_back(&head);
+      head.forward.push_back(&tail);
+    }
+  }
+  node *new_node = new node(value);
+  for(int i = 0; i <= level; i++) {
+    new_node->forward.push_back(update[i]->forward[i]);
+    update[i]->forward[i] = new_node;
+  }
+}
+
+void skiplist::remove(int value) {
+  vector<node*> update(head.level() + 1);
+  node *current = &head;
+  for(int level = head.level(); level >= 0; level--) {
+    while(current->forward[level] != &tail &&
+          current->forward[level]->value < value) {
+      current = current->forward[level];
+    }
+    update[level] = current;
+  }
+  if(current->forward[0]->value != value) return;
+
+  node *being_removed = current->forward[0];
+  for(int level = 0; level <= being_removed->level(); level++) {
+    update[level]->forward[level] = being_removed->forward[level];
+  }
+  delete being_removed;
+
+  while(head.level() > 0 && head.forward.back() == &tail) {
+    head.forward.pop_back();
+  }
+}
+
+string skiplist::print() {
+  string output;
+  for(int level = head.level(); level >= 0; level--) {
+    node *current = &head, *current_0 = &head;
+    while(current != &tail) {
+      output += (current == &head ? "#" : to_string(current->value));
+      current = current->forward[level];
+      while(current_0 != current) {
+        output += "\t";
+        current_0 = current_0->forward[0];
+      }
+    }
+    output += "#\n";
+  }
+  return output;
+}
+
+int skiplist::randomLevel() {
+  int level = 0;
+  while(rand() % 2 == 0) level++;
+  return level;
+}
